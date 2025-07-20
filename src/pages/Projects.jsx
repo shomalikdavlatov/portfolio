029e79fdc5f66ef55ecc38ba47920fdc028594a4
Shomalik Davlatov <shomalikdavlatov@gmail.com>
2025-07-20 11:16:42 +0500

    Projects page edited

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -25,12 +25,12 @@ export default function Projects() {
   ];
 
   return (
-    <section className="max-w-6xl mx-auto px-4 sm:px-6 py-10 sm:py-10">
-      <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-10 sm:mb-12 text-neutral-900 dark:text-white">
+    <section className="max-w-6xl mx-auto px-4 sm:px-6 py-8 sm:py-8">
+      <h2 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center mb-8 sm:mb-8 text-neutral-900 dark:text-white">
         {t("myprojects.title")}
       </h2>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 sm:gap-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 sm:gap-8">
         {projects.map((project, index) => (
           <div
             key={index}
